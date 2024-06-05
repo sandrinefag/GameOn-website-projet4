2cd5499e3f7d533c9c6943ae05533fee2ed65c59
sandrine fagour <sandrine94320@hotmail.fr>
2024-06-05 09:13:42 +0200

    fonction validate du formulaire fonctionnelle

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,210 +1,203 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const sendBtn = document.querySelector(".btn-submit");
 const formData = document.querySelectorAll(".formData");
 const inputForm = document.querySelectorAll("input.text-control");
-// console.log(inputForm);
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = "block";
 }
 //------------------------------------------------------------------------------
 //fermer le modal
 
-
 function modalClose() {
-  let cross = document.querySelector(".close");
-  cross.addEventListener("click", () => {
-  modalbg.style.display = "none";
-  })
+	let cross = document.querySelector(".close");
+	cross.addEventListener("click", () => {
+		modalbg.style.display = "none";
+
+	});
 }
 modalClose();
 
+let firstName = inputForm[0];
+let lastName = inputForm[1];
+let email = inputForm[2];
+let birthdate = inputForm[3];
+let contestParticipation = inputForm[4];
 
-let firstName = formData[0]
 
-let lastName = formData[1];
-let email = formData[2];
-let birthdate = formData[3];
-let contestParticipation = formData[4];
 
 
 firstName.addEventListener("input", (event) => {
-  // console.log(event.target.value);
-  const userInput = event.target.value;
-  const isValid = checkedUserName(userInput);
+	// console.log(event.target.value);
+	const userInput = event.target.value;
+	const isValid = checkedUserName(userInput);
 
-  if (isValid) {
-    removeErrorMsg(event.target);
-  } else {
-    setErrorMsg(event.target);
-  }
-})
+	if (isValid) {
+		removeErrorMsg(event.target);
+
+	} else {
+		setErrorMsg(event.target);
+	}
+});
 
 lastName.addEventListener("input", (event) => {
-  // console.log(event.target.value);
-  const userInput = event.target.value;
-  const isValid = checkedUserName(userInput);
-  
-  if (isValid) {
-    removeErrorMsg(event.target);
-  } else {
-    setErrorMsg(event.target);
-  }
-})
+	// console.log(event.target.value);
+	const userInput = event.target.value;
+	const isValid = checkedUserName(userInput);
+
+	if (isValid) {
+		removeErrorMsg(event.target);
+	} else {
+		setErrorMsg(event.target);
+	}
+});
 
 email.addEventListener("input", (event) => {
-  // console.log(event.target.value);
-  const userInput = event.target.value;
-  const isValid = checkedUserMail(userInput);
-  
-  if (isValid) {
-    removeErrorMsg(event.target);
-  } else {
-    setErrorMsg(event.target);
-  }
-})
+	// console.log(event.target.value);
+	const userInput = event.target.value;
+	const isValid = checkedUserMail(userInput);
+
+	if (isValid) {
+		removeErrorMsg(event.target);
+	} else {
+		setErrorMsg(event.target);
+	}
+});
 
 birthdate.addEventListener("input", (event) => {
-  const userInput = event.target.value;
-  const isValid = checkedBirthDateUser(userInput);
-  
-  if (isValid) {
-    removeErrorMsg(event.target);
-  } else {
-    setErrorMsg(event.target);
-  }
+	const userInput = event.target.value;
+	const isValid = checkedBirthDateUser(userInput);
 
-})
+	if (isValid) {
+		removeErrorMsg(event.target);
+	} else {
+		setErrorMsg(event.target);
+	}
+});
 
 contestParticipation.addEventListener("input", (event) => {
-  userInput = event.target.value;
-  const isValid = userNberOfParticipationGame(userInput);
-  if (isValid) {
-    removeErrorMsg(event.target);
-  } else {
-    setErrorMsg(event.target);
-  }
-})
+	userInput = event.target.value;
+	const isValid = userNberOfParticipationGame(userInput);
+	if (isValid) {
+		removeErrorMsg(event.target);
+	} else {
+		setErrorMsg(event.target);
+	}
+});
 
+sendBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+	// let validationModal = document.querySelector("form");
 
+	const isOk = isFormvalidate();
+	if (isOk) {
+		
+		console.log("validation réussie")
+	} else {
+		console.log("echec")
+	}
+});
 
 //--------------------------------------------------------------------------------
 //verification chmps prenom ok
 function checkedUserName(nameUser) {
-  console.log(nameUser);
-  let regexNames = new RegExp("^[a-zA-ZÀ-ÖØ-öøç]{2,15}[-]{0,1}[a-zA-ZÀ-ÖØ-öøç]{0,15}$");
-  if (!regexNames.test(nameUser)) {
-    // errorMessage();
-    return false;
-  }
-  return true;
+	let regexNames = new RegExp(
+		"^[a-zA-ZÀ-ÖØ-öøç]{2,15}[-]{0,1}[a-zA-ZÀ-ÖØ-öøç]{0,15}$"
+	);
+	return regexNames.test(nameUser)
 }
-
 
 function setErrorMsg(elm) {
-  console.log({elm});
-
-  elm.nextElementSibling.classList.add("active");
-
+	elm.nextElementSibling.classList.add("active");
 }
 function removeErrorMsg(elm) {
-  elm.nextElementSibling.classList.remove("active");
-
+	elm.nextElementSibling.classList.remove("active");
 }
-
-
-
-
 
 //----------------------------------------------------------
 //vérification chps email ok
 function checkedUserMail(userMail) {
-  let regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
-  if (!regexMail.test(userMail)) {
-    return false;
-  }
-  return true;
+	let regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+	return regexMail.test(userMail);
 }
-
 
 //-------------------------------------------------------------------------------
 //verification chps date de naissance ok
 function checkedBirthDateUser(userBirthDay) {
-  console.log(userBirthDay);
-  const userDate = new Date(userBirthDay);
-  console.log(userDate.getTime());
- 
-   if (!isNaN(userDate.getTime())) {
-    console.log("ok");
-    const currentDate = new Date();
-    const userAge = currentDate.getFullYear() - userDate.getFullYear();
-    const monthOfBirthUser = userDate.getMonth();
-    const dateNumberOfUser = userDate.getDate();
-    console.log(dateNumberOfUser);
-    console.log(monthOfBirthUser)
-    console.log(userAge);
+	const userDate = new Date(userBirthDay);
+	if (!isNaN(userDate.getTime())) {
+		const currentDate = new Date();
+		const userAge = currentDate.getFullYear() - userDate.getFullYear();
+		const monthOfBirthUser = userDate.getMonth();
+		const dateNumberOfUser = userDate.getDate();
 
-    if ((userAge === 18 && currentDate.getMonth() >= monthOfBirthUser && currentDate.getDate() >= dateNumberOfUser) || (userAge > 18)) {
-      return true;
-    } 
-  }
-  return false;
+		if (
+			(userAge === 18 &&
+				currentDate.getMonth() >= monthOfBirthUser &&
+				currentDate.getDate() >= dateNumberOfUser) ||
+			userAge > 18
+		) {
+			return true;
+		}
+	}
+	return false;
 }
 
-function userNberOfParticipationGame(userParticipation){
-  let regexParticipGame = /^\d{0,2}$/;
-  if (!regexParticipGame.test(userParticipation)) {
-    return false;
-  } 
-  return true;
-}
 
+
+function userNberOfParticipationGame(userParticipation) {
+	let regexParticipGame = /^\d{0,2}$/;
+	if (!regexParticipGame.test(userParticipation)) {
+		return false;
+	}
+	return true;
+}
 
 //-------------------------------------------------------------
 
-// function errorMsg() {
+function isFormvalidate() {
+	const isValidFirstName = checkedUserName(firstName.value);
+	const isValidLastName = checkedUserName(lastName.value);
+	const isValidEmail = checkedUserMail(email.value);
+	const isValidBirth = checkedBirthDateUser(birthdate.value);
+	const isValidParticipation = userNberOfParticipationGame(contestParticipation.value);
+	const btnTest = document.querySelectorAll('.checkbox-input[type="radio"]');
+	const checkedConditionFacul = document.querySelectorAll('.checkbox-input[type=checkbox]');
+	isTownSelected = false;
+	conditionValid = false
+	
+	for (let i = 0; i < btnTest.length; i++){
+		if (btnTest[i].checked) {
+			townNameChooseByUser = btnTest[i].value;
+			isTownSelected = true;
+			break;	
+		}
+	}
 
-// //msg erreur
-//   let errorMsgNameSurname = "Veuillez saisir minimum 2 charactères"
-//   let errorMsgmail = "Le mail n'est pas valide";
-//   let errorMsgbirth = "chps incomplet";
-//   let errorParticipation = "Veuillez entrer un nombre entre 0 et 99";
+	for (let i = 0; i < checkedConditionFacul.length; i++){
+		if (checkedConditionFacul[0].checked && (checkedConditionFacul[1].checked || !checkedConditionFacul[1].checked)) {
+			conditionValid = true;
+			break;
+		}
+	}
+	return isValidFirstName && isValidEmail && isValidBirth && isValidLastName && isValidParticipation && isTownSelected && conditionValid;
 
-// }
-  //afficher msg
-function displayErrorMsg(errorMessage) {
-  const errorMsgTag = document.getElementById("errorMsg");
- 
-  if (!errorMsgTag ) {
-    const errorMsgTag = document.createElement("p");
-    errorMsgTag.id = "errorMsg"
-    
-    errorMsgTag.innerText = errorMessage;
-  } else {
-    errorMsgTag.remove()
-  }
-
+	
 }
-function validate() {
-  let validForm = document.querySelector("form");
-  validForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-  
-  })
 
-}
+
 
